76000bfcb11ecd22c06693adb437ac54b2c3bfb1
Moteyabai <134784925+Moteyabai@users.noreply.github.com>
2023-07-21 15:41:53 +0700

    Repo and db connection

---
+++ Repositories/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repositories.Models
+{
+    public partial class Car
+    {
+        public int CarId { get; set; }
+        public string CarName { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Image { get; set; } = null!;
+    }
+}
---
+++ Repositories/Models/CarShopContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace Repositories.Models
+{
+    public partial class CarShopContext : DbContext
+    {
+        public CarShopContext()
+        {
+        }
+
+        public CarShopContext(DbContextOptions<CarShopContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Car> Cars { get; set; } = null!;
+        public virtual DbSet<Role> Roles { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer(GetConnectionString());
+            }
+        }
+
+        public string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfiguration configuration = builder.Build();
+            return configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.Property(e => e.CarId).HasColumnName("CarID");
+
+                entity.Property(e => e.CarName).HasMaxLength(50);
+
+                entity.Property(e => e.Category).HasMaxLength(30);
+
+                entity.Property(e => e.Image).HasMaxLength(50);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.ToTable("Role");
+
+                entity.Property(e => e.RoleId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("RoleID");
+
+                entity.Property(e => e.Role1)
+                    .HasMaxLength(10)
+                    .HasColumnName("Role");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Password).HasMaxLength(30);
+
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+                entity.Property(e => e.UserName).HasMaxLength(50);
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.RoleId)
+                    .HasConstraintName("FK_Users_Role");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
