d0c12bd288f8455844baf3917785d3613b58985b
Moteyabai <134784925+Moteyabai@users.noreply.github.com>
2023-07-21 15:52:10 +0700

    Generate CRUD Product, User

---
+++ CarShopApp/Pages/Cars/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Repositories.Models;
+
+namespace CarShopApp.Pages.Cars
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Repositories.Models.CarShopContext _context;
+
+        public CreateModel(Repositories.Models.CarShopContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Car Car { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Cars == null || Car == null)
+            {
+                return Page();
+            }
+
+            _context.Cars.Add(Car);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ CarShopApp/Pages/Cars/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Models;
+
+namespace CarShopApp.Pages.Cars
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Repositories.Models.CarShopContext _context;
+
+        public IndexModel(Repositories.Models.CarShopContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Car> Car { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Cars != null)
+            {
+                Car = await _context.Cars.ToListAsync();
+            }
+        }
+    }
+}
